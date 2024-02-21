0362da79cadb5e37329c3d912e21cded58f0117d
UladzislauM <marshal25@yandex.by>
2024-02-21 08:22:40 +0300

    dev_not_cor: implemented order and user service

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,42 @@
+﻿using TestTask.Data.Interfaces;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    /// <summary>
+    /// Buisness logic for work with the order model
+    /// </summary>
+    public class OrderService : IOrderService
+    {
+        private readonly IRepository<Order> _orderRepository;
+
+        public OrderService(IRepository<Order> orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        /// <summary>
+        /// Find the order with the largest order amount
+        /// </summary>
+        /// <returns>Task<Order></returns>
+        public Task<Order> GetOrder()
+        {
+            Task<Order> order = _orderRepository.Get();
+
+            return order;
+        }
+
+        /// <summary>
+        /// Find orders in which the quantity of the product is more than 10
+        /// </summary>
+        /// <returns>Task<List<Order>></returns>
+        public Task<List<Order>> GetOrders()
+        {
+            Task<List<Order>> orders = _orderRepository.GetEntities();
+
+            return orders;
+        }
+
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,42 @@
+﻿using TestTask.Data.Interfaces;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    /// <summary>
+    /// Buisness logic for work with the user model
+    /// </summary>
+    public class UserService : IUserService
+    {
+        private readonly IRepository<User> _userRepository;
+
+        public UserService(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        /// <summary>
+        /// Find the user with the largest number of orders
+        /// </summary>
+        /// <returns>Task<User></returns>
+        public Task<User> GetUser()
+        {
+            Task<User> user = _userRepository.Get();
+
+            return user;
+        }
+
+        /// <summary>
+        /// Find users with Inactive status
+        /// </summary>
+        /// <returns>Task<List<User>></returns>
+        public Task<List<User>> GetUsers()
+        {
+            Task<List<User>> users = _userRepository.GetEntities();
+
+            return users;
+        }
+
+    }
+}
